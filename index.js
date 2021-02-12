3d24779d19b91028a350b8df73020d04d6e81592
Erik Zhao <erikzhao98@gmail.com>
2021-02-12 03:13:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function Driver(name, age, experience) {
     this.experience = experience
 }
 
-function PickUpLocation(address, city) {
+function PickupLocation(address, city) {
     this.address = address
     this.city = city
 }
