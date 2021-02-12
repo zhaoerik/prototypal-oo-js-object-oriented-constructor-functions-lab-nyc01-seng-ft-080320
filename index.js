d62abf3ddac3a660e6de5b325b4a36659d6e2aeb
Erik Zhao <erikzhao98@gmail.com>
2021-02-12 03:09:51 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function Scooters(year, color, model) {
+    this.year = year,
+    this.color = color,
+    this.model = model
+}
+
+function Drivers(name, age, experience) {
+        name,
+        age,
+        experience
+}
+
+function PickUpLocation(address, city) {
+        address
+        city
+}
